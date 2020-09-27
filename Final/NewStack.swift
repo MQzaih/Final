e485226faff0e25ca06554bb0d246da0e9faeb4a
MQzaih <masaqzaih@gmail.com>
2020-09-27 10:23:30 +0300

    Extra is added

---
+++ Final/NewStack.swift
@@ -0,0 +1,141 @@
+//
+//  NewStack.swift
+//  Final
+//
+//  Created by Asal 2 on 27/09/2020.
+//  Copyright © 2020 Asal 2. All rights reserved.
+//
+
+
+
+import Foundation
+
+class NewStack <T: Hashable & Comparable>{
+    var maximumCapacity:Int?
+    var valuesOfStack = [T] ()
+  private  var set = Set<T>()
+    init(size: Int){
+        maximumCapacity=size
+        valuesOfStack.reserveCapacity(_: size)
+      
+    }
+    
+/* func Distinct() {
+        valuesOfStack = Array (Set(valuesOfStack))
+       
+    }*/
+    
+    private func addToSet (elemnt: T) -> Bool{
+
+        if set.contains(elemnt) {
+               print("It is a duplicate value")
+           // print(set)
+            return false
+        }
+            
+        else{
+            set.insert(elemnt)
+            print("Successfully added")
+          //  print(set)
+            return true
+
+        }
+    }
+    
+func sortElements(){
+    valuesOfStack = valuesOfStack.sorted(by: { $0 < $1 } ) //placeholder to value
+        
+        
+    }
+    
+    
+    func insertAt (index: Int , value: T){
+         if maximumCapacity != valuesOfStack.count {
+        let check = addToSet(elemnt: value)
+        if check {
+            valuesOfStack.insert(value, at: index)
+        }
+            
+        }
+         else {
+            print("Maximum Capacity, Can not be added")
+        }
+    }
+    
+    func removeAt (index: Int){
+        let trash = valuesOfStack[index]
+        set.remove(trash)
+      valuesOfStack.remove(at: index)
+    }
+    
+    func resizingStack(capacity: Int){
+        maximumCapacity = capacity
+        valuesOfStack.reserveCapacity(_:capacity)
+    }
+     
+  
+    func push (value: T){
+      //  Distinct()
+        if maximumCapacity != valuesOfStack.count {
+      let check =  addToSet(elemnt: value)
+        if check {
+            valuesOfStack.append(value)
+        }
+        }
+        else {
+            print("Reached Maximum Capacity, Can not be added")
+        }
+    }
+    
+    func pop (){
+        if isEmpty() {
+            print("Stack is Empty")
+        }
+        let trash = peek()
+        set.remove(trash!)
+        valuesOfStack.removeLast()
+
+        
+    }
+    func peek()-> T? {
+        if isEmpty(){
+            print("Stack is Empty")
+            return nil
+        }
+        return valuesOfStack.last;
+    }
+    
+    func isEmpty()-> Bool {
+        if valuesOfStack.count == 0
+        {
+            return true
+        }
+        return false
+    }
+   
+}
+
+class Node: Hashable, Comparable  {
+    static func < (lhs: Node, rhs: Node) -> Bool {
+         lhs.id < rhs.id
+    }
+    
+    static func == (lhs: Node, rhs: Node) -> Bool {
+            lhs.id == rhs.id
+       }
+    
+   var hashValue:Int{
+        return  id.hashValue ^ description.hashValue
+    }
+    let id = Int.random(in: 1..<1000)
+    let description : String?
+   var check:String {
+        if description != nil {
+            return description!
+        }
+        return description!
+    }
+    init(titleOfJob:String) {
+description = titleOfJob
+    }
+}
--- Final/main.swift
+++ Final/main.swift
@@ -6,7 +6,88 @@
 //  Copyright © 2020 Asal 2. All rights reserved.
 //
 
+
 import Foundation
 
-print("Hello, World!")
+var intgrStk = NewStack <Int> (size: 4)
+intgrStk.push(value : 7)
+intgrStk.push(value : 7)
+intgrStk.push(value : 3)
+intgrStk.push(value : 4)
+intgrStk.push(value : 4)
+intgrStk.push(value : 3)
+intgrStk.push(value : 8)
+intgrStk.resizingStack(capacity: 7)
+intgrStk.push(value : 81)
+intgrStk.push(value : 89)
+
+intgrStk.insertAt(index: 2, value: 1)
+
+intgrStk.sortElements()
+print(intgrStk.valuesOfStack)
+intgrStk.removeAt(index: 2)
+intgrStk.resizingStack(capacity: 10)
+
+intgrStk.push(value: 6)
+intgrStk.push(value: 33)
+
+intgrStk.insertAt(index: 4, value: 1)
+print(intgrStk.valuesOfStack)
+
+while !(intgrStk.isEmpty()){
+    let testing = intgrStk.peek()!
+    print(testing )
+    intgrStk.pop()
+}
+print(intgrStk.valuesOfStack)
+
+/*
+var employeesOfCompany = Stack <Node>(size: 5)
+print(employeesOfCompany.valuesOfStack)
+var employee1 = Node(titleOfJob:"Director")
+employeesOfCompany.push(value: employee1)
+var employee2 = Node(titleOfJob:"CEO")
+employeesOfCompany.push(value: employee2)
+var employee3 = Node(titleOfJob:"Developer")
+employeesOfCompany.push(value: employee3)
+print(employeesOfCompany.valuesOfStack.description)
+while !(employeesOfCompany.isEmpty()){
+    let testing = employeesOfCompany.peek()!
+    print(testing.description )
+    employeesOfCompany.pop()
+}
+*/
+
+print("////////////Queue////////////")
+var intgrQ = NewQueue <Int> (size: 4)
+intgrQ.add(value : 7)
+intgrQ.add(value : 7)
+intgrQ.add(value : 3)
+intgrQ.add(value : 4)
+intgrQ.add(value : 4)
+intgrQ.add(value : 3)
+intgrQ.add(value : 8)
+intgrQ.resizingStack(capacity: 7)
+intgrQ.add(value : 81)
+intgrQ.add(value : 89)
+
+intgrQ.insertAt(index: 2, value: 1)
+
+intgrQ.sortElements()
+print(intgrQ.valuesOfQueue)
+intgrQ.removeAt(index: 2)
+intgrQ.resizingStack(capacity: 10)
+
+intgrQ.add(value: 6)
+intgrQ.add(value: 33)
+
+intgrQ.insertAt(index: 4, value: 1)
+print(intgrQ.valuesOfQueue)
+
+while !(intgrQ.isEmpty()){
+    let testing = intgrQ.dequeue()
+    print(testing )
+    intgrQ.remove()
+}
+print(intgrQ.valuesOfQueue)
 
